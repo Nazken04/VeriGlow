577def2b817af9477d53abc3bc212c493c3dc0e0
Nazken04 <71633984+Nazken04@users.noreply.github.com>
2025-05-18 19:38:27 +0500

    Delete backend directory

--- backend/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    mongoURI: process.env.MONGO_URI,
-    jwtSecret: process.env.JWT_SECRET,
-    contractAddress: process.env.CONTRACT_ADDRESS,
-    infuraProjectID: process.env.INFURA_PROJECT_ID,
-    walletPrivateKey: process.env.WALLET_PRIVATE_KEY,
-  };
-  
